4295d674449ea481df247a5d7e525203f28b67ed
Viktor Ulyankin <freeman.viktor@mail.ru>
2021-10-22 21:22:37 +0300

    Поменял названия example тестов.

--- test/hermione/example.hermione.js
+++ test/hermione/example.hermione.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 
-describe('github', async function() {
+describe('[H] [EXAMPLE] github', async function() {
     it('Тест, который пройдет', async function() {
         await this.browser.url('https://github.com/gemini-testing/hermione');
         await this.browser.assertView('plain', '#readme', {
--- test/unit/example.test.js
+++ test/unit/example.test.js
@@ -1,4 +1,4 @@
-describe('Simple Test Case', () => {
+describe(' [EXAMPLE] Simple Test Case', () => {
     it('Should return 4', () => {
         expect(2 + 2).toBe(4);
     });
